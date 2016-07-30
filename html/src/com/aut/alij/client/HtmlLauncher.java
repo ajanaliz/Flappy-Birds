f998e989926378f7f0254426b96bd02092929f44
ajanaliz <jchbbst@gmail.com>
2016-07-30 10:16:23 +0430

    initial commit

---
+++ html/src/com/aut/alij/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.aut.alij.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.aut.alij.FlappyBirds;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new FlappyBirds();
+        }
+}
